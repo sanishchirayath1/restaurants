b816ef33c6a8dbb715f09ea50e7868b5d3aa5f0e
sanishchirayath1 <sanishchirayath@gmail.com>
2021-12-08 19:40:59 +0530

    data file added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
+import data from "./data";
+
 
 function App() {
+    console.log(data.restaurants)
     return (
         <div className="App">
         <h1>Hello CodeSandbox</h1>
