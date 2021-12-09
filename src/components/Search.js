a34c2da1e54238f2c590bfe0e913fa527f2987ac
sanishchirayath1 <sanishchirayath@gmail.com>
2021-12-09 14:31:53 +0530

    location update

--- src/components/Search.js
+++ src/components/Search.js
@@ -14,41 +14,19 @@ function Search() {
        }
 
     let placeCoords = {
-        lat:10.523958,
-        lng:76.212888
+        lat:10.5818,
+        lng:76.0359
     }
     console.log(placeCoords)
     console.log(coords)
 
 
-    function haversine(lat1, lon1, lat2, lon2)
-    {
-        // distance between latitudes
-        // and longitudes
-        let dLat = (lat2 - lat1) * Math.PI / 180.0;
-        let dLon = (lon2 - lon1) * Math.PI / 180.0;
-           
-        // convert to radiansa
-        lat1 = (lat1) * Math.PI / 180.0;
-        lat2 = (lat2) * Math.PI / 180.0;
-         
-        // apply formulae
-        let a = Math.pow(Math.sin(dLat / 2), 2) +
-                   Math.pow(Math.sin(dLon / 2), 2) *
-                   Math.cos(lat1) *
-                   Math.cos(lat2);
-        let rad = 6371;
-        let c = 2 * Math.asin(Math.sqrt(a));
-        return rad * c;
-         
-    }
-     // Driver Code
-     let lat1 = coords.lat;
-     let lon1 = coords.lng;
-     let lat2 = placeCoords.lat;
-     let lon2 = placeCoords.lng;
+    let haversine = require("haversine-distance");
+    let haversineM = haversine(coords, placeCoords); //Results in meters (default)
+    let haversineKm = haversineM /1000; //Results in kilometers
 
-     
+    console.log("distance (in meters): " + haversineM + "m");
+    console.log("distance (in kilometers): " + haversineKm+ "km");
 
     navigator.geolocation.getCurrentPosition(pos => {
         setCoords({
@@ -67,7 +45,7 @@ function Search() {
                 <div style={{color:"#fff"}}>
                     <p>Your Latitude: {coords.lat}</p>
                     <p>Your Longitude: {coords.lng}</p>
-                    <p>You are {haversine(lat1, lon1, lat2, lon2)} KM away from Thrissur</p>
+                    <p>You are {haversineKm} KM away from Thrissur</p>
                 </div>
             )}
         </>
